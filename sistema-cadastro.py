d22f4038afb4612385b210f871736c04045fed62
SousaDev <emersonsousa735@gmail.com>
2026-07-03 21:38:30 -0300

    Corrigi erros de listas e identações, adicionado comments de cada função e adicionei loop de verificação de contas salvas no arquivo para login

--- sistema-cadastro.py
+++ sistema-cadastro.py
@@ -1,3 +1,9 @@
+# SISTEMA DE CADASTRO DE USUÁRIOS #
+# O sistema permite que os usuários criem uma conta, façam login e armazenem seus dados em um arquivo de texto. O sistema também valida o e-mail e a senha do usuário durante o cadastro.
+# FORAM UTILZADAS ESTRUTURAS DE REPETIÇÃO, CONDIÇÕES E TRATAMENTO DE ERROS PARA GARANTIR A FUNCIONALIDADE DO SISTEMA.
+
+
+# MENSAGEM DE BOAS VINDAS #
 print ("=" * 50)
 print (" " * 10 + "Bem Vindo ao Sistema de Cadastro")
 print ("=" * 50)
@@ -12,13 +18,13 @@ while True:
         print("3 - Sair do sistema")
         opcao = input("Digite a opção desejada: ")
     except ValueError:
-        print("Digite uma opção válida.")
+        print("Certifique-se de digitar apenas números.")
         continue
 
      # OPÇÃO DE CADASTRAR CONTA #
     if opcao == "1":
         print("=" * 50)
-        print(" " * 10 + "Crie sua conta")
+        print(" " * 18 + "Crie sua conta")
         print("=" * 50)
 
         # LOOP PARA CADASTRO DE EMAIL #
@@ -49,52 +55,45 @@ while True:
             else:
                 break
 
-         # MENSAGEM DE BOAS VINDAS #
+         # MENSAGEM DE BOAS VINDAS APÓS CADASTRO DE CONTA #
         nome = input("Digite o seu nome de usuário: ")
         print("Cadastro realizado com sucesso!")
         print(f"Bem-vindo(a), {nome}!")
 
+        # SALVANDO OS DADOS DO USUÁRIO EM UM ARQUIVO DE TEXTO #
         arquivo = open("usuarios.txt", "a")
         arquivo.write(f"{email},{senha},{nome}\n")
         arquivo.close()
 
+    # OPÇÃO DE LOGIN #
     elif opcao == "2":
         print("=" * 50)
-        print(" " * 10 + "Faça o login do email e senha cadastrados")
+        print(" " * 13 + "Faça o login da sua conta")
         print("=" * 50)
-
-        login_email = input("Digite o seu e-mail: ")
-        login_senha = input("Digite a sua senha: ")
-
-        arquivo = open("usuarios.txt", "r")
-        for linha in arquivo:
-            email_salvo, senha_salva, nome_salvo = linha.strip().split(",")
-            if login_email == email_salvo and login_senha == senha_salva:
-                print(f"Login realizado com sucesso! Bem-vindo(a), {nome_salvo}!")
-                break
-        else:
-            print("E-mail ou senha incorretos. Tente novamente.")
-                #COLOCR LOOP PARA VERIFICAR SE ESTÁ CORRETO#
-        arquivo.close()
         
+        # LOOP PARA LOGIN DE USUÁRIO #
+        while True:
+            login_email = input("Digite o seu e-mail: ")
+            login_senha = input("Digite a sua senha: ")
+
+            # VERIFICAÇÃO DE LOGIN EM ARQUIVO #
+            arquivo = open("usuarios.txt", "r")
+            for linha in arquivo:
+                email_salvo, senha_salva, nome_salvo = linha.strip().split(",")
+                if login_email == email_salvo and login_senha == senha_salva:
+                    print(f"Login realizado com sucesso! Bem-vindo(a), {nome_salvo}!")
+                    break
+                else:
+                    print("E-mail ou senha incorretos. Tente novamente.")
+                    continue
+            arquivo.close()
+
+    # OPÇÃO DE SAIR DO SISTEMA #    
     elif opcao == "3":
         print("Saindo do sistema...")
         break
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Criar sistema para salvamento de usuários
+    # TRATAMENTO DE OPÇÃO INVÁLIDA #
+    else:
+        print("Opção inválida. Digite uma opção válida.")
+        continue
